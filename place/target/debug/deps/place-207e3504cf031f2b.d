b7c055e83c3a13bfa5592173c136984f08eaf440
Brian Bordeaux <bbo@ad.unc.edu>
2025-01-27 02:21:21 +0000

    finished example

---
+++ place/target/debug/deps/place-207e3504cf031f2b.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/place/target/debug/deps/place-207e3504cf031f2b: src/main.rs
+
+/workspaces/rust-tutorial/place/target/debug/deps/place-207e3504cf031f2b.d: src/main.rs
+
+src/main.rs:
---
+++ place/target/debug/place.d
@@ -0,0 +1,1 @@
+/workspaces/rust-tutorial/place/target/debug/place: /workspaces/rust-tutorial/place/src/main.rs
